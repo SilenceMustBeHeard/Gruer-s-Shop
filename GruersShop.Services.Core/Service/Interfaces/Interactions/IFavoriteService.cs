4f0ac945f9783327b60d1ec44c0f8acbfda57798
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-04-12 06:09:11 +0300

    Move product views, update routes and interface

--- GruersShop.Services.Core/Service/Interfaces/Interactions/IFavoriteService.cs
+++ GruersShop.Services.Core/Service/Interfaces/Interactions/IFavoriteService.cs
@@ -9,7 +9,7 @@ public interface IFavoriteService
 {
 
 
-    Task<IEnumerable<ProductViewModel>> GetUserFavoritesAsync(string userId);;
+    Task<IEnumerable<ProductViewModel>> GetUserFavoritesAsync(string userId);
     Task<bool> ToggleFavoriteAsync(string userId, Guid productId);
     Task<bool> IsFavoriteAsync(string userId, Guid productId);
 }
