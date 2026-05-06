f723aa4dd9742117e41ae326fbd5a22ba9e14351
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-05-06 15:46:32 +0300

    Added ProductDetailsControllerApi

---
+++ GruersShop.API.Web/Controllers/Areas/User/Products/ProductDetailsControllerApi.cs
@@ -0,0 +1,38 @@
+﻿using GruersShop.Services.Core.Service.Interfaces.Bakery;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace GruersShop.API.Web.Controllers.Areas.User.Products;
+
+[Route("api/[controller]")]
+[ApiController]
+[AllowAnonymous]
+public class ProductDetailsControllerApi : ControllerBase
+{
+    private readonly ICatalogClientService _catalogClientService;
+
+    public ProductDetailsControllerApi(ICatalogClientService catalogClientService)
+    {
+        _catalogClientService = catalogClientService;
+    }
+
+    [HttpGet]
+    public virtual async Task<IActionResult> Index(Guid id)
+    {
+        string? userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+       
+
+        var model = await _catalogClientService.GetProductDetailsViewModelAsync(id, userId);
+
+        if (model == null)
+        {
+          
+            return NotFound();
+        }
+
+        return Ok(model);
+    }
+}
